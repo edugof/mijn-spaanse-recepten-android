83595af4fc0368b3bf4529465a42975c19eb24e9
Eduardo Gomez <edugof@gmail.com>
2017-03-07 21:45:05 +0100

    Initial commit

---
+++ app/src/main/java/com/edugof/mijnspaanserecepten/DisplayDessertsActivity.java
@@ -0,0 +1,13 @@
+package com.edugof.mijnspaanserecepten;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DisplayDessertsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_desserts);
+    }
+}
