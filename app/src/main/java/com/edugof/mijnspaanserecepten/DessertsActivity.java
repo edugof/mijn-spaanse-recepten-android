35107b4ba7db50c117ce7123d114187a006eb144
Eduardo Gomez <edugof@gmail.com>
2017-03-10 18:42:42 +0100

    Change to a more appropriate layout for recipes. Send recipe id as reference to Recipe Activity.

--- app/src/main/java/com/edugof/mijnspaanserecepten/DessertsActivity.java
+++ app/src/main/java/com/edugof/mijnspaanserecepten/DessertsActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 public class DessertsActivity extends AppCompatActivity {
 
@@ -11,9 +13,21 @@ public class DessertsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_desserts);
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent,
+                                    View view,
+                                    int position,
+                                    long recipeId) {
+                requestRecipe(recipeId);
+            }
+        });
     }
 
-    public void requestRecipe(View view) {
-        startActivity(new Intent(this, RecipeActivity.class));
+    public void requestRecipe(long recipeId) {
+        Intent intent = new Intent(this, RecipeActivity.class);
+        intent.putExtra("recipeId", recipeId);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/edugof/mijnspaanserecepten/RecipeActivity.java
+++ app/src/main/java/com/edugof/mijnspaanserecepten/RecipeActivity.java
@@ -1,7 +1,9 @@
 package com.edugof.mijnspaanserecepten;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class RecipeActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class RecipeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+        Intent myIntent = getIntent();
+        Long recipeId = myIntent.getLongExtra("recipeId", 0);
+        String recipeName = getResources().getResourceEntryName(recipeId.intValue());
+
+        Toast.makeText(RecipeActivity.this, "" + recipeName, Toast.LENGTH_SHORT).show();
     }
 }
